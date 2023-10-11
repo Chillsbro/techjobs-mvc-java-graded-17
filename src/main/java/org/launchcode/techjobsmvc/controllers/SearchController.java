5bd860275d408ff7c4078d52edec72f33d29dd99
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-10-10 23:09:22 -0500

    complete bonus mission 1

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -39,6 +39,7 @@ public class SearchController {
         }
         model.addAttribute("jobs",jobs);
         model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("searchType",searchType);
         return "search";
     }
 
